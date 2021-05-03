591df56ffb09bd2a316fcc4f7c805abfe56f35a8
Radhika A <ragarwal865@student.fuhsd.org>
2021-05-03 08:55:52 -0700

    added labyrinth and gridtemplate

---
+++ SAR/src/GridTemplate.java
@@ -0,0 +1,151 @@
+import java.awt.Point;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+import processing.core.PApplet;
+
+public abstract class GridTemplate {
+
+	/** Add a field to represent the grid. This time, make it a 2D array of characters. **/
+	protected char[][] grid;
+	
+	
+	
+	/**
+	 * Construct an empty 2D array with some default dimensions.
+	 */
+	public GridTemplate() {
+		grid = new char[20][20];
+	}
+	
+	
+	/**
+	 * Construct an empty 2D array with dimensions width and height, then fill it with data from the file filename.
+	 * 
+	 * @param width The width of the grid.
+	 * @param height The height of the grid.
+	 * @param filename The text file to read from.
+	 */
+	public GridTemplate(int width, int height, String filename) {
+		grid = new char[height][width];
+		readData(filename, grid);
+	}
+	
+	
+	/**
+	 * 
+	 * Code a toString() method that nicely prints the grid to the commandline.
+	 * 
+	 */
+	public String toString() {
+		String string = "   ";
+		int rows = grid[0].length;
+		int columns = grid.length;
+		for(int a = 0; a <rows; a++) {
+			if (a<10) {
+				string = string + a;
+			}else {
+				string = string + a%10;
+			}
+		}
+		for (int i = 0; i<rows; i++) {
+			if (i<10) {
+				string = string + "\n " + i + " ";
+			}else {
+				string = string + "\n" + i + " ";
+			}
+			for(int j = 0; j<columns; j++) {
+				string = string + grid[i][j];
+				
+			}
+		}
+		return string;
+	}
+	
+	
+	/**
+	 * (Graphical UI)
+	 * Draws the grid on a PApplet.
+	 * The specific way the grid is depicted is up to the coder.
+	 * 
+	 * @param marker The PApplet used for drawing.
+	 * @param x The x pixel coordinate of the upper left corner of the grid drawing. 
+	 * @param y The y pixel coordinate of the upper left corner of the grid drawing.
+	 * @param width The pixel width of the grid drawing.
+	 * @param height The pixel height of the grid drawing.
+	 */
+	public void draw(PApplet marker, float x, float y, float width, float height) {
+		float r = (float)Math.min((double)width, (double)height);
+		r = r/grid.length;
+		for(int i = 0; i<grid.length; i++) {
+			for(int j = 0; j<grid.length; j++) {
+				marker.fill(255);
+				marker.square(x + (r*i), y + (j*r), r);
+			}
+		}
+	}
+
+
+	
+	
+	/**
+	 * (Graphical UI)
+	 * Determines which element of the grid matches with a particular pixel coordinate.
+	 * This supports interaction with the grid using mouse clicks in the window.
+	 * 
+	 * @param p A Point object containing a graphical pixel coordinate.
+	 * @param x The x pixel coordinate of the upper left corner of the grid drawing. 
+	 * @param y The y pixel coordinate of the upper left corner of the grid drawing.
+	 * @param width The pixel width of the grid drawing.
+	 * @param height The pixel height of the grid drawing.
+	 * @return A Point object representing a coordinate within the grid.
+	 */
+	public Point clickToIndex(Point p, float x, float y, float width, float height) {
+		float r = (float)Math.min((double)width, (double)height);
+		r = r/grid.length;
+		if (p.x>= r*(grid.length) || p.y>= r*(grid.length) || p.x< 0 || p.y<0) {
+			return null;
+		}
+		int indexX = (int)((p.x - x)/r);
+		int indexY = (int)((p.y - y)/r);
+		Point coordinate = new Point(indexX, indexY);
+		return coordinate;
+	}
+	
+
+
+	public void readData (String filename, char[][] gameData) {
+		File dataFile = new File(filename);
+
+		if (dataFile.exists()) {
+			int count = 0;
+
+			FileReader reader = null;
+			Scanner in = null;
+			try {
+					reader = new FileReader(dataFile);
+					in = new Scanner(reader);
+					
+					while (in.hasNext()) {
+						String line = in.nextLine();
+						for(int i = 0; i < line.length(); i++)
+							if (count < gameData.length && i < gameData[count].length)
+								gameData[count][i] = line.charAt(i);
+
+						count++;
+					}
+
+			} catch (IOException ex) {
+				throw new IllegalArgumentException("Data file " + filename + " cannot be read.");
+			} finally {
+				if (in != null)
+					in.close();
+			}
+			
+		} else {
+			throw new IllegalArgumentException("Data file " + filename + " does not exist.");
+		}
+	}
+}
