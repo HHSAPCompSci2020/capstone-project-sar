3954939d27b648bd5652553f620acef8eaf9976d
Ayush <69985088+Asaran135@users.noreply.github.com>
2021-05-06 11:00:01 -0700

    PosionArrow
    fix take 2

--- SAR/src/projectiles/PoisonArrow.java
+++ SAR/src/projectiles/PoisonArrow.java
@@ -3,4 +3,9 @@ package projectiles;
 
 public class PoisonArrow extends StandardProjectile{
 
+	public PoisonArrow(int x, int y, int moveSpeed, int dir) {
+		super(x, y, moveSpeed, dir);
+		
+	}
+
 }
