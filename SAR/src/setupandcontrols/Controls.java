da51ee63aecff09d7367fa42409cedc3477cb011
Shachaf <shachaf.smith@gmail.com>
2021-05-06 10:31:43 -0700

    add starter stuff on controls

--- SAR/src/setupandcontrols/Controls.java
+++ SAR/src/setupandcontrols/Controls.java
@@ -1,9 +1,21 @@
 package setupandcontrols;
 
+import java.util.Scanner;
+import processing.core.PApplet;
+
+
 /**
  * Manages ability to use keyboard and mouse for certain actions
  *@version 5/5
  */
-public class Controls {
+public class Controls extends PApplet {
 
+	Scanner kBoard = new Scanner(System.in);
+	
+	/**
+	 * Makes something happen when the mouse is dragged
+	 */
+	public void mouseDragged() {
+		
+	}
 }
