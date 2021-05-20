fe2016fe809c4fc8b00ab10dbff12d3b02ccca61
Radhika A <ragarwal865@student.fuhsd.org>
2021-05-19 20:37:04 -0700

    finished with maze!!!

--- SAR/src/setupandcontrols/DrawingSurface.java
+++ SAR/src/setupandcontrols/DrawingSurface.java
@@ -44,7 +44,7 @@ public class DrawingSurface extends PApplet {
 
 
 	public DrawingSurface() {
-		board = new Maze("mazeLevels/test2.txt");
+		board = new Maze("mazeLevels/test3.txt");
 		yPos = height / 2;
 		obstacle = new WaterWall(10, getyPos());
 		obstacle1 = new WaterWall(10, getyPos());
